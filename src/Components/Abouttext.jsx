a8bf83a2678312819ba792e98ec28406e4f9c11e
ayan-plush <ayankhajuria2005@gmail.com>
2024-08-01 20:12:56 +0530

    content for about us was changed

--- src/Components/Abouttext.jsx
+++ src/Components/Abouttext.jsx
@@ -9,7 +9,18 @@ function Abouttext(){
         <img src='/assets/logo.jpg' className='h-[40vh] py-[5vh] w-[30vh]'></img>
         </div>
       <div className=' text-[2vh] tracking-wide px-[4.3vh] uppercase font-light text-center w-full'>
-      Our mission is to celebrate diversity by exploring literature from various cultures, backgrounds, and perspectives. Here, we believe in the power of stories to foster empathy, understanding, and connection. Whether you are an avid reader or new to the literary world, our club offers a safe and welcoming space to discuss, learn, and grow together. Join us as we embark on a journey through the pages of books that reflect the richness of the human experience, and become part of a community that values inclusivity and respect for all.
+      Welcome to Sahitya Book Club, a warm and close-knit community of passionate readers hailing from diverse backgrounds, all united by a shared love for literature. At Sahitya, we believe that books are more than just stories; they are bridges that connect us across cultures, experiences, and generations. Our members come from every walk of life, each bringing unique perspectives that enrich our discussions and deepen our understanding of the world.
+
+Each month, we come together to explore a thoughtfully curated selection of books, spanning from timeless classics to contemporary gems. Our gatherings are more than book discussions—they are vibrant conversations where ideas are exchanged, friendships are forged, and minds are opened. Whether we are delving into the intricacies of a literary masterpiece or discovering the hidden treasures of an emerging author, our aim is to foster a sense of belonging and mutual respect among our members.
+
+At Sahitya Book Club, we celebrate the power of stories to inspire, challenge, and transform us. We invite you to join us on this literary journey, to share your insights, listen to others, and become a part of our vibrant community. Together, let's explore the boundless world of literature and create lasting memories through the pages we turn.
+
+
+
+
+
+
+
       </div>
       <div className='text-[5vh] font-light text-center mt-[5vh] flex justify-center uppercase  tracking-widest text-[#47382B]'>
         <h1>OUR current favourites</h1>
