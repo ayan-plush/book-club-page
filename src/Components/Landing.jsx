129d7f7d72c361c0e472b4bac09d21296c609304
ayan-plush <ayankhajuria2005@gmail.com>
2024-12-11 20:27:14 +0530

    redesign

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -9,7 +9,7 @@ function Landing() {
       <a href="https://forms.gle/7wtz3nwse96pwXCCA" className="text-[10vh] text-[#47382B] flex justify-center tracking-tight font-[impacted] font-bold ">
       <img src='/assets/JOINUS.png' className='h-[9.75vh] border-b-[0.1vh] border-b-[#47362D] w-[28.5vh]'></img>
       </a>
-      <h7 className="flex justify-center font-bold text-[#907e5a] text-[1.5vh] uppercase tracking-tighter ">connect. read. converse.</h7>
+      <div className="flex justify-center font-bold text-[#907e5a] text-[1.5vh] uppercase tracking-tighter ">connect. read. converse.</div>
       <div className=' flex justify-center'>
         <img src='/assets/theeye1.png' className='h-[45vh] w-[45vh]'></img>
       </div> 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,8 +7,9 @@ function Navbar() {
       <div className="  h-[7vh] w-full px-[5vw] py-[3vh]  text-[#47382B] font-medium flex justify-between items-center">
       
     <Link to="/" className="text-[2vh] ">HOME</Link>
-    <Link to="/aboutus" className="text-[2vh] ">ABOUT US</Link>
+    <Link to="/aboutus" className="text-[2vh] ">OUR PURPOSE</Link>
     <Link to="https://forms.gle/7wtz3nwse96pwXCCA" className="text-[2vh] ">JOIN US</Link>
+    <Link to="/aboutus" className="text-[2vh] ">LOG IN</Link>
       
     </div>
     </div>
